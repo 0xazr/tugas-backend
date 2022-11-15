2031adc4a383cec215b16acf307f4e07a3489b8e
ssudo <ssudo@duck.com>
2022-11-15 09:37:27 +0700

    add validation

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 import bodyParser from 'body-parser';
 import express from 'express';
 import cors from 'cors';
+import { body, validationResult } from 'express-validator';
 
 const app = express();
 const port = 3000;
@@ -10,27 +11,25 @@ app.use(bodyParser.json());
 
 import firebase from 'firebase/compat/app';
 import 'firebase/compat/firestore';
-import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
-
+import { getAuth, signInWithEmailAndPassword } from 'firebase/auth';
 
 // TODO: Replace the following with your app's Firebase project configuration
 // See: https://firebase.google.com/docs/web/learn-more#config-object
 const firebaseConfig = {
-  apiKey: "AIzaSyBgZYJoUhN2if_IeCB-RQB2Hz_yV_ZCfV8",
-  authDomain: "simple-todo-app-c5a3b.firebaseapp.com",
-  projectId: "simple-todo-app-c5a3b",
-  storageBucket: "simple-todo-app-c5a3b.appspot.com",
-  messagingSenderId: "740962103721",
-  appId: "1:740962103721:web:d847f722b57dc69d2894b8",
-  measurementId: "G-J6X7HZLJ46"
+  apiKey: 'AIzaSyBgZYJoUhN2if_IeCB-RQB2Hz_yV_ZCfV8',
+  authDomain: 'simple-todo-app-c5a3b.firebaseapp.com',
+  projectId: 'simple-todo-app-c5a3b',
+  storageBucket: 'simple-todo-app-c5a3b.appspot.com',
+  messagingSenderId: '740962103721',
+  appId: '1:740962103721:web:d847f722b57dc69d2894b8',
+  measurementId: 'G-J6X7HZLJ46',
 };
-
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
 const auth = getAuth(firebaseApp);
 
-const email = "admin@tutorial-backend.gov";
-const password = "s3cretp4ssw0rd";
+const email = 'admin@tutorial-backend.gov';
+const password = 's3cretp4ssw0rd';
 
 signInWithEmailAndPassword(auth, email, password)
   .then((userCredential) => {
@@ -39,8 +38,7 @@ signInWithEmailAndPassword(auth, email, password)
   .catch((error) => {
     const errorCode = error.code;
     const errorMessage = error.message;
-  }
-);
+  });
 
 const db = firebaseApp.firestore();
 
@@ -66,8 +64,12 @@ app.get('/users', (req, res) => {
   }
 });
 
-app.post('/users', (req, res) => {
+app.post('/users', body('email').isEmail(), (req, res) => {
   try {
+    const error = validationResult(req);
+    if (!error.isEmpty()) {
+      return res.status(400).json({ errors: error.array() });
+    }
     var nama = req.body.nama;
     var email = req.body.email;
 
